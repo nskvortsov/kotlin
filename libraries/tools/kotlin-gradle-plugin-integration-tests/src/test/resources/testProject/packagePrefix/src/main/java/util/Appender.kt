8ceffe092e6340e40afc98a236b3cfc136b46016
Nikita Skvortsov <nikita.skvortsov@jetbrains.com>
2017-04-10 11:14:38 +0300

    add simple integration test project

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/packagePrefix/src/main/java/util/Appender.kt
@@ -0,0 +1,6 @@
+package my.pack.name.util
+
+
+class Appender {
+    fun append(arg: String): String = "Hello, $arg"
+}
---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/packagePrefix/src/main/java/util/JUtil.java
@@ -0,0 +1,8 @@
+package my.pack.name.util;
+
+
+public class JUtil {
+    public static String useless() {
+        return "I am useless";
+    }
+}
